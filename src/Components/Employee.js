19dd6aae37f6c2d7b9c96a8e45d975989dafd77f
shubhamsalke0321 <salkeshubham0321@gmail.com>
2025-03-12 11:58:40 +0530

    first commit

---
+++ src/Components/Employee.js
@@ -0,0 +1,32 @@
+const Employee=[
+    {
+        id:'1',
+        uname:'shubham salke',
+        age:'21',
+        desg:'Frontend Developer',
+        salary:'34000'
+    },
+    {
+        id:'2',
+        uname:'Prasad Langhe',
+        age:'25',
+        desg:'Full stack Developer',
+        salary:'45000'
+    },
+    {
+        id:'3',
+        uname:'Aditya Gadekar',
+        age:'23',
+        desg:'python Developer',
+        salary:'40000'
+    },
+    {
+        id:'4',
+        uname:'Nitin Katore',
+        age:'27',
+        desg:'Angular Developer',
+        salary:'50000'
+    }
+]
+
+export default Employee
